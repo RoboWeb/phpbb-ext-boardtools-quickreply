67fbddbfe70799cf8b60ffa561e8ac33afbd1a42
Tomasz Wolny <ykee.tomasz.wolny@gmail.com>
2020-09-10 18:29:23 +0200

    fix: ucp form with QucikReply options, and two translations

--- language/pl/quickreply_ucp.php
+++ language/pl/quickreply_ucp.php
@@ -25,6 +25,6 @@ $lang = array_merge($lang, array(
 	'QR_ENABLE_AJAX_PAGINATION'  => 'Nie odświeżaj szybkiej odpowiedzi kiedy poruszasz się po tematach',
 	'QR_ENABLE_SCROLL'           => 'Włącz automatyczne przewijanie gdy przechodzisz między stronami tematu.',
 	'QR_ENABLE_SOFT_SCROLL'      => 'Włącz delikatne przewijanie i animację kiedy przechodzisz między stronami tematu i gdy wyślesz szybką odpowiedź.',
-	'QR_ENABLE_WARNING'          => 'Warn if the entered quick reply can be lost',
-	'QR_FIX_EMPTY_FORM'          => 'Allow to fix quick reply form when it is empty',
+	'QR_ENABLE_WARNING'          => 'Ostrzegaj, jeśli wprowadzona szybka odpowiedź, może być utracona',
+	'QR_FIX_EMPTY_FORM'          => 'Pozwól na zamocowanie formularza szybkiej odpowiedzi do krawędzi ekranu, nawet kiedy jest pusty',
 ));
